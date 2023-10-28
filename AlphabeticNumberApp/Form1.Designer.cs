226f2f5fd7f2bcb14773bc619d2ed552db94a9d3
Nelson-Lamounier <lamounierleao@gmail.com>
2023-10-28 14:16:33 +0100

    Add project files.

---
+++ AlphabeticNumberApp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace AlphabeticNumberApp
+{
+    partial class AlphNum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBoxInput = new System.Windows.Forms.MaskedTextBox();
+            this.labelInpu = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.textBoxDisp = new System.Windows.Forms.TextBox();
+            this.labelDisp = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxInput
+            // 
+            this.maskedTextBoxInput.Location = new System.Drawing.Point(219, 21);
+            this.maskedTextBoxInput.Mask = "000-LLL-LLLL";
+            this.maskedTextBoxInput.Name = "maskedTextBoxInput";
+            this.maskedTextBoxInput.Size = new System.Drawing.Size(178, 31);
+            this.maskedTextBoxInput.TabIndex = 0;
+            // 
+            // labelInpu
+            // 
+            this.labelInpu.AutoSize = true;
+            this.labelInpu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInpu.Location = new System.Drawing.Point(12, 31);
+            this.labelInpu.Name = "labelInpu";
+            this.labelInpu.Size = new System.Drawing.Size(201, 16);
+            this.labelInpu.TabIndex = 1;
+            this.labelInpu.Text = "Enter Alphabetic Phone Number:";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.Location = new System.Drawing.Point(276, 69);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(121, 28);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "Get Phone Number";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // textBoxDisp
+            // 
+            this.textBoxDisp.Location = new System.Drawing.Point(220, 136);
+            this.textBoxDisp.Name = "textBoxDisp";
+            this.textBoxDisp.Size = new System.Drawing.Size(177, 31);
+            this.textBoxDisp.TabIndex = 3;
+            // 
+            // labelDisp
+            // 
+            this.labelDisp.AutoSize = true;
+            this.labelDisp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDisp.Location = new System.Drawing.Point(101, 144);
+            this.labelDisp.Name = "labelDisp";
+            this.labelDisp.Size = new System.Drawing.Size(112, 18);
+            this.labelDisp.TabIndex = 4;
+            this.labelDisp.Text = "Phone Number:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(15, 175);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // AlphNum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(412, 210);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.labelDisp);
+            this.Controls.Add(this.textBoxDisp);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.labelInpu);
+            this.Controls.Add(this.maskedTextBoxInput);
+            this.Name = "AlphNum";
+            this.Text = "Alphabetic Phone Number";
+            this.Load += new System.EventHandler(this.AlphNum_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxInput;
+        private System.Windows.Forms.Label labelInpu;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox textBoxDisp;
+        private System.Windows.Forms.Label labelDisp;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ AlphabeticNumberApp/Form1.cs
@@ -0,0 +1,94 @@
+﻿/*
+A, B, and C = 2
+D, E, and F = 3
+G, H, and I = 4
+J, K, and L = 5
+M, N, and O = 6
+P, Q, R, and S = 7
+T, U, and V = 8
+W, X, Y, and Z = 9
+
+Create an application that lets the user enter a 10-character telephone number in the format XXX-XXX-XXXX. The application should display the telephone number with any alphabetic characters that appeared
+in the original translated to their equivalent. For exemple, if the user enters 555-GET-FOOD, the application must display 555-438-3663
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphabeticNumberApp
+{
+    public partial class AlphNum : Form
+    {
+        public AlphNum()
+        {
+            InitializeComponent();
+        }
+        //Form level variable - character jagged array(An array of arrays) placing characters from A to
+        char[][] alphabetic =
+        {
+            new char[] { ' ' }, // First level  which represents the zero index that way index two in my arrray corresponds exactly with the number two on the keypad
+            new char[] { ' ' }, // Otherwise, if starting from Zero, that is to avoid to compensate for the difference that the array starts on KEypod start with two ( counter ++)
+            new char[] { 'A', 'B', 'C'},
+            new char[] { 'D', 'E', 'F'},
+            new char[] { 'G', 'H', 'I'},
+            new char[] { 'J', 'K', 'L'},
+            new char[] { 'M', 'N', 'O'},
+            new char[] { 'P', 'Q', 'R', 'S'},
+            new char[] { 'T', 'U', 'V'},
+            new char[] { 'W', 'X', 'Y', 'Z'}
+        };
+
+        
+
+        private void AlphNum_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            // Variables to manipulate users input
+            string numAlpha = maskedTextBoxInput.Text; // String to gather user input
+            string codeArea = numAlpha.Substring(0, 3) + "-";// Default Code Area
+            string phoneLetters = numAlpha.Substring(4);// Get users inputted letters after area code
+            string phoneNum = codeArea; // Phone number generated
+
+            // Nested loop to loop the letters inputted
+            foreach(char c in phoneLetters)
+            {
+                for(int i = 2; i < alphabetic.Length; i++) // Starting at 2 beacause 1 and 2 are not being used - Loop untill the end
+                {
+                    // Capture each row of the letters array - Length is unknow of the rows each need to be capture separateky
+                    char[] arrayInner = alphabetic[i];
+                    for (int j = 0; j < arrayInner.Length; j++)
+                    {
+                        // Comparing user inputted
+                        if (char.ToUpper(c) == arrayInner[j])
+                        {
+                            if (phoneNum.Length == 7)
+                                phoneNum += "-";// Adding the characther 'dash'
+
+                            // The indexes of the array are mapped to the letter indexex in the letter array
+                            phoneNum += i.ToString();
+                        }
+                           
+                    }
+                }
+            }
+
+            // Display the phone number
+            textBoxDisp.Text = phoneNum;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
